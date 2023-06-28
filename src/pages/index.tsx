3c8f0f777402cab20199f69080f6fe6aab2e7793
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-06-28 18:40:47 +0100

    sign in buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,16 +15,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-         <SignInButton />
-         <SignOutButton />
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+        
+          <div className="flex gap-3 text-slate-400">
+            <SignInButton />
+            <SignOutButton />
+          </div>
+         
       </main>
     </>
   );
