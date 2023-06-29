f72d6b77ef6ea710acd3adaa61ed7e6c6f0f9636
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-06-29 10:02:02 +0100

    adding auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-     publicRoutes: ["/"]
+
+         publicRoutes: ["/"]
 });
 
 export const config = {
+
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
